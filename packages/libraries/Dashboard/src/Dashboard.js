ae70dc66304af743c6071fee5be5380c1ae63244
Guy Hilton <guyjohnhilton@gmail.com>
2024-06-20 11:17:28 +0100

    Initial setup - hello world (#1)
    * initial import
    
    * package json
    
    * naming
    
    * rearrage directories
    
    * more oackagige naming
    
    * naming
    
    * hello world app
    
    * hello world styling
    
    * componentise tools
    
    * library component poc
    
    ---------
    
    Co-authored-by: gswirrl <guy@swirrl.com>

---
+++ packages/libraries/Dashboard/src/Dashboard.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export const Dashboard = props => <div>
+	<h1>Dashboard tool</h1>
+	<p>Hello world</p>
+</div>
---
+++ packages/libraries/Validate/src/Validate.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {LoremIpsum} from "@tpx/LoremIpsum"
+
+export const Validate = props => <div>
+	<h1>Validate tool</h1>
+	<LoremIpsum />
+</div>
