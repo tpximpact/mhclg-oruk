196e060b2ddb888bcd709633d55a06546273cdb6
Guy Hilton <gjhltn@users.noreply.github.com>
2024-06-21 23:03:20 +0100

    refactor file reading

---
+++ packages/apps/hub/src/util/content.js
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import { join } from 'path'
+import { PATHS } from './paths'
+
+export const slugsFrom = (folder, extension) =>
+	allFilesOfType(getPath(folder), extension).map(f => ({ slug: f }))
+
+const getPath = dir => join(process.cwd(), PATHS.contentRoot, dir)
+
+const allFilesOfType = (path, fileExtension) => {
+	if (fs.existsSync(path)) {
+		const result = fs.readdirSync(path).filter(f => f.split('.')[1] === fileExtension)
+		return result
+	}
+}
+
+export const readFile = ({ file, folder }) => {
+	const path = getPath(folder)
+	const filePath = join(path, file)
+	return fs.readFileSync(filePath, 'utf8')
+}
