8fefbdab96c87fb5ccd3024df1ba657308fd9683
gswirrl <guyjohnhilton@gmail.com>
2024-06-22 23:56:26 +0100

    neutral css

--- packages/apps/hub/src/app/page.js
+++ packages/apps/hub/src/app/page.js
@@ -1,10 +1,9 @@
-import styles from './page.module.css'
 import Link from 'next/link'
 import { LoremIpsum } from '@tpx/LoremIpsum'
 
 export default function Home() {
 	return (
-		<main className={styles.main}>
+		<main>
 			<h1>Open Referral UK</h1>
 			<LoremIpsum />
 			<Link href='/dashboard'>Dashboard</Link>
