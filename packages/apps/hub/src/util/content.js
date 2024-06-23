b2b0df4c054c4dcb2eb8e03596e90684b9f19cc6
gswirrl <guyjohnhilton@gmail.com>
2024-06-23 12:06:42 +0100

    how

--- packages/apps/hub/src/util/content.js
+++ packages/apps/hub/src/util/content.js
@@ -3,6 +3,27 @@ import { join } from 'path'
 import { PATHS } from './paths'
 import { FILE_EXTENSION } from './markdown'
 
+export const buildItemMenuData = dir =>
+	allFilesOfType(getPath(dir), FILE_EXTENSION)
+		.filter(f => f !== 'index.md')
+		.map(item => ({
+			target: fileNameToTarget(item),
+			text: fileNameToText(item)
+		}))
+
+const fileNameToTarget = fileName => fileName.split('.')[0]
+const fileNameToText = fileName => {
+	let result = fileName.split('.')[0]
+	const regex = /^[0-9]*/i
+	result = result.replace(regex, '')
+	result = result.replace('-', '')
+	result = result.replaceAll('-', ' ')
+	result = result.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g, function (c) {
+		return c.toUpperCase()
+	})
+	return result
+}
+
 export const slugsFrom = folder =>
 	allFilesOfType(getPath(folder), FILE_EXTENSION).map(f => ({ slug: f.split('.')[0] }))
 
