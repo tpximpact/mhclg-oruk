c5eeab1f20e42998d76c7ef80dfbd5289139081e
gswirrl <guyjohnhilton@gmail.com>
2024-06-27 21:28:42 +0100

    developer section header

--- packages/apps/hub/src/components/Header/index.js
+++ packages/apps/hub/src/components/Header/index.js
@@ -2,8 +2,8 @@ import styles from './Header.module.css'
 import { Logo } from '@/components/Logo'
 import { Navigation } from '@/components/Navigation'
 
-export const Header = ({ selected }) => (
-	<header className={styles.header}>
+export const Header = ({ selected, developer }) => (
+	<header className={`${styles.header} ${developer && styles.developer }` }>
 		<Logo />
 		<Navigation selected={selected} />
 	</header>
