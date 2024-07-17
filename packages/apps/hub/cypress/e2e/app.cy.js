190d23d6f37d6878904b94f455342958a4da5389
g.j.hilton <guyjohnhilton@gmail.com>
2024-07-17 21:53:09 +0100

    cypress init

---
+++ packages/apps/hub/cypress/e2e/app.cy.js
@@ -0,0 +1,15 @@
+describe('Navigation', () => {
+    it('should navigate to the dashboard', () => {
+      cy.visit('http://localhost:3000/')
+         cy.get('a[href*="dashboard"]').click()
+      cy.url().should('include', '/developer/tools/dashboard')
+      cy.get('h1').contains('Dashboard')
+    })
+
+    it('should navigate to the validator', () => {
+      cy.visit('http://localhost:3000/')
+         cy.get('a[href*="validator"]').click()
+      cy.url().should('include', '/developer/tools/validator')
+      cy.get('h1').contains('Validator')
+    })
+  })
