554b64d7ca50586299a27834f33e4fcced435174
g.j.hilton <guyjohnhilton@gmail.com>
2024-07-23 03:19:06 +0100

    first cut menu

--- packages/libraries/Validator/src/Validator.js
+++ packages/libraries/Validator/src/Validator.js
@@ -5,13 +5,13 @@ import { Main } from '@tpx/Main'
 
 export const Validator = ({ validationAction }) => (
 	<Main>
-		<PageMargin >
-		<form action={validationAction}>
-			<label>
-				GUID (For now enter any string of charcters you like! )<input type='text' name='id' />
-			</label>
-			<button>Submit</button>
-		</form>
+		<PageMargin>
+			<form action={validationAction}>
+				<label>
+					GUID (For now enter any string of charcters you like! )<input type='text' name='id' />
+				</label>
+				<button>Submit</button>
+			</form>
 		</PageMargin>
 	</Main>
 )
