8704aa9f7dd2b52bf7768a687fba6ab865e713bc
g.j.hilton <guyjohnhilton@gmail.com>
2024-07-23 02:51:36 +0100

    sketchy placeholder content

--- packages/libraries/Validator/src/Validator.js
+++ packages/libraries/Validator/src/Validator.js
@@ -1,14 +1,17 @@
 'use client'
 
+import { PageMargin } from '@tpx/PageMargin'
 import { Main } from '@tpx/Main'
 
 export const Validator = ({ validationAction }) => (
 	<Main>
+		<PageMargin >
 		<form action={validationAction}>
 			<label>
-				GUID: <input type='text' name='id' />
+				GUID (For now enter any string of charcters you like! )<input type='text' name='id' />
 			</label>
 			<button>Submit</button>
 		</form>
+		</PageMargin>
 	</Main>
 )
