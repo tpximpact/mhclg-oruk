0278df3af50dbcae1709a000e249661adb16a86b
g.j.hilton <guyjohnhilton@gmail.com>
2024-08-02 13:42:25 +0100

    icons etc

---
+++ packages/apps/hub/src/components/ValidationResult/LiteralResponse.js
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import {  jsonLanguage  } from '@codemirror/lang-json';
+import { vscode} from '@uiw/codemirror-theme-vscode';
+import {EditorView } from "@codemirror/view"
+
+const json = `{
+        "wang": "chung",
+        "everybodyg": "tonight",
+}
+` 
+
+const customTheme = EditorView.theme({
+    $: {
+        fontSize: "21pt"
+      }
+})
+
+export const LiteralResponse = () =>
+<div>
+  <h2>JSON response received from www.example.com</h2>
+<CodeMirror  theme={vscode} value={json} height="200px" extensions={[jsonLanguage,customTheme]} />;</div>
