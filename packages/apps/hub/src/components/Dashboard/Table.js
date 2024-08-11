a6194e289a9033a33df379e0a03e9fbe7804b1df
g.j.hilton <guyjohnhilton@gmail.com>
2024-08-12 00:47:27 +0100

    dashbiard details

--- packages/apps/hub/src/components/Dashboard/Table.js
+++ packages/apps/hub/src/components/Dashboard/Table.js
@@ -1,10 +1,16 @@
 import styles from './Dashboard.module.css'
 import Link from 'next/link'
 
-const formatDate = (dateString) => {
-	const options = { hour:"numeric", minute:"numeric", year: "2-digit", month: "2-digit", day: "numeric" }
+const formatDate = dateString => {
+	const options = {
+		hour: 'numeric',
+		minute: 'numeric',
+		year: '2-digit',
+		month: '2-digit',
+		day: 'numeric'
+	}
 	return new Date(dateString).toLocaleDateString(undefined, options)
-  }
+}
 
 export const Table = ({ headers, data }) => (
 	<table className={styles.table}>
@@ -23,17 +29,20 @@ export const Table = ({ headers, data }) => (
 	</table>
 )
 
-const Row = ({ data }) => (
-	<tr>
-		<th>{data.label}</th>
-		<td>1.0</td>
-		<td>{data.isUp ? 'pass' : 'fail'}</td>
-		<td>{data.isServicesValid ? 'pass' : 'fail'}</td>
-		<td>{data.isSearchEnabled ? 'pass' : 'fail'}</td>
-		<td>{formatDate(data.lastCheck)}</td>
+const Row = ({ data }) => {
+	const detailsURL = `/developer/tools/dashboard/${data.id}`
+	return (
+		<tr>
+			<th>{data.label}</th>
+			<td>1.0</td>
+			<td>{data.isUp ? 'pass' : 'fail'}</td>
+			<td>{data.isServicesValid ? 'pass' : 'fail'}</td>
+			<td>{data.isSearchEnabled ? 'pass' : 'fail'}</td>
+			<td>{formatDate(data.lastCheck)}</td>
 
-		<td>
-			<Link href={data.label}>details</Link>
-		</td>
-	</tr>
-)
+			<td>
+				<Link href={detailsURL}>details</Link>
+			</td>
+		</tr>
+	)
+}
---
+++ packages/apps/hub/src/components/DashboardDetails/index.js
@@ -0,0 +1,1 @@
+export { DashboardDetails } from './DashboardDetails'
