54ec40b30baf80f2db92ac1d553e574a8bf2cd50
g.j.hilton <guyjohnhilton@gmail.com>
2024-09-26 02:23:36 +0100

    sort Directory

--- packages/apps/hub/src/components/Dashboard/Dashboard.js
+++ packages/apps/hub/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,5 @@
-'use client'
-
-import {useState} from 'react'
-import { PaginatedTable } from '@/components/PaginatedTable'
-import { TableSorting, getSortingOptions, getSortedRows } from '@/components/TableSorting'
 import { generate } from './generateDummyData'
-
+import { SortedAndPaginatedTable } from '@/components/SortedAndPaginatedTable'
 
 // TODO real data !
 
@@ -14,34 +9,12 @@ const data = generate({
 	failEveryNRows: 3
 })
 
-export const Dashboard = ({ currentPage }) => {
+export const Dashboard = (props) => {
 	const view = data.definitions.views.dashboard
-	const headers = data.definitions.columns
-	const [sortBy,setSortBy] = useState(view.defaultSortBy.column)
-	const [sortedRows,setSortedRows] = useState(data.data)
-	const sortingOptions = getSortingOptions(view,data)
 	
-const changeSort = newVal => {
-	setSortBy(newVal)
-	let newSortedRows = getSortedRows(newVal,data)
-	// newSortedRows = newSortedRows.reverse()
-	setSortedRows(newSortedRows)
-}
-
-	return (
-		<>
-		<TableSorting 
-		 values={sortingOptions}
-		 selectedValue={sortBy}
-		 onValueChange={changeSort}
-		/>
-		<PaginatedTable
-			rowsPerPage={view.rowsPerPage}
-			columns={view.columns}
-			headers={headers}
-			rows={sortedRows}
-			currentPage={currentPage}
-		/>
-		</>
-	)
+	return <SortedAndPaginatedTable
+	view={view}
+	tableData={data}
+	{...props}
+	/>
 }
