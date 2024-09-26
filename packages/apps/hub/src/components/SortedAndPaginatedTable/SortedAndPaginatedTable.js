6896b369ce9097f28556b8306a324a0c56509d6e
g.j.hilton <guyjohnhilton@gmail.com>
2024-09-26 13:16:46 +0100

    first cut ast ascending / descending

--- packages/apps/hub/src/components/SortedAndPaginatedTable/SortedAndPaginatedTable.js
+++ packages/apps/hub/src/components/SortedAndPaginatedTable/SortedAndPaginatedTable.js
@@ -1,1 +1,63 @@
-export {SortedAndPaginatedTable} from './SortedAndPaginatedTable'
+
+'use client'
+
+import {useState, useEffect} from 'react'
+import { PaginatedTable } from '@/components/PaginatedTable'
+import { DIRECTION, TableSorting, getSortingOptions, getSortedRows } from '@/components/TableSorting'
+
+export const SortedAndPaginatedTable = ({
+	currentPage,
+	tableData,
+	view
+}) => {
+	const headers = tableData.definitions.columns
+	const [sortBy,setSortBy] = useState(view.defaultSortBy.column)
+	const [sortDirection,setSortDirection] = useState(DIRECTION.ASCENDING)
+    const [sortedRows,setSortedRows] = useState(tableData.data)
+	const sortingOptions = getSortingOptions(view,tableData)
+	
+const sort = () => {
+    alert("sorting by " + sortBy)
+    const data = JSON.parse(JSON.stringify(tableData))
+    let newSortedRows = getSortedRows(sortBy,data)
+        
+    if (sortDirection===DIRECTION.DESCENDING) {
+            newSortedRows = newSortedRows.reverse()
+        }
+    setSortedRows(newSortedRows)
+}
+
+    useEffect(() => {
+     sort()
+        
+      }, [sortBy, sortDirection]);
+
+const changeSort = newVal => {
+	setSortBy(newVal)
+}
+
+const changeDirection = newVal => {
+	setSortDirection(newVal)
+    sort()
+}
+
+	return (
+		<>
+		<TableSorting 
+		 values={sortingOptions}
+		 selectedValue={sortBy}
+         selectedDirection = {sortDirection}
+		 onValueChange={changeSort}
+         onDirectionChange={changeDirection}
+		/>
+		<PaginatedTable
+			rowsPerPage={view.rowsPerPage}
+			columns={view.columns}
+			headers={headers}
+			rows={sortedRows}
+			currentPage={currentPage}
+		/>
+		</>
+	)
+}
+
