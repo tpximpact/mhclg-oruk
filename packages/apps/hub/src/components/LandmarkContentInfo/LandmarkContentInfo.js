2fae65437b7eec9ea7b7c688061159a7a3d1efa2
g.j.hilton <guyjohnhilton@gmail.com>
2024-10-26 00:01:26 +0100

    remive zeros

--- packages/apps/hub/src/components/LandmarkContentInfo/LandmarkContentInfo.js
+++ packages/apps/hub/src/components/LandmarkContentInfo/LandmarkContentInfo.js
@@ -5,11 +5,11 @@ import Link from 'next/link'
 export const LandmarkContentInfo = ({ showNav }) => (
 	<footer role='contentInfo' className={styles.footer}>
 		<div className={styles.bottom}>
-			{showNav && (
+			{showNav ? (
 				<PageMargin>
 					<Link href='/sitemap'>Sitemap</Link>
 				</PageMargin>
-			)}
+			) : null}
 			<PageMargin>Copyright © 2024 Open Referral UK</PageMargin>
 		</div>
 	</footer>
