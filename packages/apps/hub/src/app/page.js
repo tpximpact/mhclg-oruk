9da5a9ab0a55bde2a080ac7acdff5416f5721f6c
Guy Hilton <guy.hilton@tpximpact.com>
2024-10-28 16:12:09 +0000

    Development (#22)
    * homepage demo mode switch
    
    * import {configValueToBoolean} from '@/util/configValueToBoolean'
    
    * placeholder homepage copy
    
    * make undefined config values false
    
    ---------
    
    Co-authored-by: Guy Hilton <gjhltn@users.noreply.github.com>

--- packages/apps/hub/src/app/page.js
+++ packages/apps/hub/src/app/page.js
@@ -1,9 +1,20 @@
 import { NamedMarkdownPage } from '@/components/NamedMarkdownPage'
+import { PageMargin } from '@tpx/PageMargin'
+import {configValueToBoolean} from '@/util/configValueToBoolean'
 
 export const metadata = {
 	title: 'Open Referral UK'
 }
 
 export default function Home() {
+	// special case just for previeding tools to user group
+	if (configValueToBoolean(process.env.TOOLS_ONLY)) {
+		return <Tools />
+	}
+
 	return <NamedMarkdownPage name='home' />
 }
+
+
+const Tools = () => 
+<PageMargin><section><h1>Open Referral UK Tools Preview</h1><p>We are developing new versions of our developer tools to accompany the revised 3.0 provile of Open Referral.</p><p>On this demomstration site, you can access work-in-progres beta versions of the tools for testing.</p><ul><li><a href="/developers/validator">Validator</a></li><li><a href="/developers/dashboard">Dashboard</a></li><li><a href="/community/directory">Directory</a></li></ul></section></PageMargin>
