012c5fa3f56e8a18c7fd487bf2a624dea5046266
g.j.hilton <guyjohnhilton@gmail.com>
2024-11-04 23:51:52 +0000

    test

--- packages/apps/hub/src/app/developers/api/page.js
+++ packages/apps/hub/src/app/developers/api/page.js
@@ -1,6 +1,8 @@
 import { PageMargin } from '@tpx/PageMargin'
 import { NamedMarkdownPage } from '@/components/NamedMarkdownPage'
-
+import SwaggerUI from 'swagger-ui-react'
+import 'swagger-ui-react/swagger-ui.css'
+import './patches.css'
 import { VersionedDocumentation } from '@/components/VersionedDocumentation'
 import { getVersions } from '@/util/versionedContent'
 export const metadata = {
@@ -15,6 +17,9 @@ export default async function Page() {
 			<NamedMarkdownPage name='api' />
 			<PageMargin>
 				<VersionedDocumentation allVersions={allVersions} contentData={contentData} />
+			</PageMargin>
+			<PageMargin>
+				<SwaggerUI url='https://petstore.swagger.io/v2/swagger.json' />
 			</PageMargin>
 		</>
 	)
