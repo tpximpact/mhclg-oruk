154986997264e0a8dcaafefe3b6c205c160c8320
Guy Hilton <gjhltn@users.noreply.github.com>
2024-11-12 12:06:20 +0000

    emend validator field placeholder and label

--- packages/apps/hub/src/components/ValidatorForm/ValidatorForm.js
+++ packages/apps/hub/src/components/ValidatorForm/ValidatorForm.js
@@ -10,11 +10,11 @@ export const ValidatorForm = ({ action, defaultValue }) => {
 		<div className={styles.form}>
 			<form action={action}>
 				<label className={styles.label}>
-					Feed URL <span aria-label='required'>(required)</span>
-					<input type='URL' defaultValue={defaultValue} name='uri' required />
+					URL
+					<input type='URL' defaultValue={defaultValue} name='uri' placeholder="Enter URL to check" />
 				</label>
 				<span className={styles.example}>
-					eg: https://oruk-api-2a920f51d6bb.herokuapp.com/api/mock
+					eg: https://example.com/my-oruk-feed
 				</span>
 				<input type='hidden' name='id' value={UUID} />
 				<Button>Check</Button>
