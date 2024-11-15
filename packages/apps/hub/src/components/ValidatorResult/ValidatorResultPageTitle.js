c00d9044bf97042f5d16cd65489c8a3fd0dac58b
Guy Hilton <gjhltn@users.noreply.github.com>
2024-11-15 12:09:46 +0000

    improve styles

--- packages/apps/hub/src/components/ValidatorResult/ValidatorResultPageTitle.js
+++ packages/apps/hub/src/components/ValidatorResult/ValidatorResultPageTitle.js
@@ -1,5 +1,5 @@
 import styles from './Title.module.css'
 
 export const ValidatorResultPageTitle = () => (
-	<h1 className={styles.pageTitle}>Service Validation results</h1>
+	<h1 className={styles.pageTitle}>Data feed compliance report</h1>
 )
