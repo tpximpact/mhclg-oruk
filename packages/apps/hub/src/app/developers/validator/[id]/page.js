39a94b16295e6666bebab50473e569a6f319fa9d
Guy Hilton <gjhltn@users.noreply.github.com>
2024-11-18 11:00:33 +0000

    sample reports

--- packages/apps/hub/src/app/developers/validator/[id]/page.js
+++ packages/apps/hub/src/app/developers/validator/[id]/page.js
@@ -21,10 +21,9 @@ export default async function Page({
 			<RemoteJSON
 				method={METHOD.POST}
 				RetryComponent={() => (
-					<section style={{ marginTop: '2rem' }}>
-						<h2 style={{ fontSize: '2rem', marginBottom: '2rem' }}>Retry validation</h2>
-						<ValidatorForm action={navigate} defaultValue={searchParams.uri} />
-					</section>
+					
+						<ValidatorForm
+					title="Re-check"	action={navigate} defaultValue={searchParams.uri} />
 				)}
 				ResultRenderComponent={ValidatorResult}
 				endpoint={process.env.VALIDATOR_ENDPOINT}
