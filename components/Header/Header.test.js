b6f07b4056487915918877853823dfa3d6d64dfc
g.j.hilton <guyjohnhilton@gmail.com>
2024-12-06 23:04:08 +0000

    re-restart

---
+++ components/Header/Header.test.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from './Header';
+
+test('renders Header component', () => {
+  const { getByText } = render(<Header />);
+  expect(getByText('Header')).toBeInTheDocument();
+});
