8971f4972b1c4008ee6d7e4233800f0b1dfa0d58
g.j.hilton <guyjohnhilton@gmail.com>
2024-12-21 08:55:18 +0000

    storybook

---
+++ components/Icon/index.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Icon, { CircledIcon, ICON_TYPE } from './index';
+
+export default {
+  title: 'Components/Icon',
+  component: Icon,
+};
+
+const Template = (args) => <Icon {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 24,
+  colour: 'black',
+  weight: 2,
+  icon: ICON_TYPE.ADD,
+};
+
+export const Circled = (args) => <CircledIcon {...args} />;
+Circled.args = {
+  size: 24,
+  colour: 'black',
+  weight: 2,
+  icon: ICON_TYPE.ADD,
+  bg: 'lightgray',
+  border: '1px solid black',
+};
