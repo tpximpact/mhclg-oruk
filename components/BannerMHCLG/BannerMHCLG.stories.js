10a348d338bd81dfa7ea76944a3a7e0985688ee8
g.j.hilton <guyjohnhilton@gmail.com>
2024-12-21 07:45:31 +0000

    simple storybooks

---
+++ components/BannerMHCLG/BannerMHCLG.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BannerMHCLG } from './BannerMHCLG';
+
+export default {
+  title: 'Components/BannerMHCLG',
+  component: BannerMHCLG,
+};
+
+export const Default = () => <BannerMHCLG />;
---
+++ components/Changelog/Changelog.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Changelog } from './Changelog';
+
+export default {
+  title: 'Components/Changelog',
+  component: Changelog,
+};
+
+const sampleData = [
+  {
+    versionNumber: '1.0.0',
+    fields: [
+      {
+        title: 'Field 1',
+        added: [{ name: 'Keyword 1' }],
+        removed: [{ name: 'Keyword 2' }],
+        replaced: [{ name: 'Keyword 3', replacedWith: 'Keyword 4' }],
+      },
+    ],
+  },
+];
+
+export const Default = () => <Changelog versions={sampleData} />;
