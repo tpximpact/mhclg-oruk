a8e9613a0338c3444fbc7a8f6eda165e8553b175
g.j.hilton <guyjohnhilton@gmail.com>
2024-12-21 03:28:34 +0000

    jsdoc

--- utilities/configValueToBoolean.js
+++ utilities/configValueToBoolean.js
@@ -1,1 +1,14 @@
-export const configValueToBoolean = v => (v ? JSON.parse(v.toLowerCase()) : false)
+/**
+ * Converts a configuration value to a boolean.
+ *
+ * @param {string} v - The configuration value to convert.
+ * @returns {boolean} - The boolean representation of the configuration value.
+ */
+export const configValueToBoolean = v => {
+	if (!v) return false
+	try {
+		return JSON.parse(v.toLowerCase())
+	} catch (e) {
+		return false
+	}
+}
