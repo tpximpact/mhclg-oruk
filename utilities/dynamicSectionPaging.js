e0ee4757e540dea24a41c3c9e69784a2b675ebdc
g.j.hilton <guyjohnhilton@gmail.com>
2024-12-21 05:58:03 +0000

    refactoring

--- utilities/dynamicSectionPaging.js
+++ utilities/dynamicSectionPaging.js
@@ -1,7 +1,10 @@
-import { fileThumbnail } from './fileThumbnail'
+import { fileThumbnail } from './fileThumbnail';
 
 export const dynamicSectionPaging = (index, allFiles, folder) => {
-	if (index < 0 || index >= allFiles.length) return null
-	const fileName = allFiles[index]
-	return fileThumbnail(folder, fileName)
-}
+  if (!Array.isArray(allFiles) || typeof index !== 'number' || typeof folder !== 'string') {
+    throw new Error('Invalid arguments');
+  }
+  if (index < 0 || index >= allFiles.length) return null;
+  const fileName = allFiles[index];
+  return fileThumbnail(folder, fileName);
+};
