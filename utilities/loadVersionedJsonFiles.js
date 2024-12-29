bed1ac48c6637837f3afb17303bf708dfba56d79
Guy Hilton <gjhltn@users.noreply.github.com>
2024-12-29 18:03:47 +0000

    work on json loading

--- utilities/loadVersionedJsonFiles.js
+++ utilities/loadVersionedJsonFiles.js
@@ -1,18 +1,22 @@
 import {getAllFilesInFolder} from './getAllFilesInFolder'
 
+const versionedContentVersionFromFilename = filename => {
+	const name = filename.split(".")[0] + ""
+	return name.replace("_",".")
+}
+
 export const loadVersionedJsonFiles = contentFolder =>
  {
 	 const files = getAllFilesInFolder(contentFolder).filter(
 	 f => f.split(".")[1] === "json"
 	)
 	 let allVersions = files.map(
-	 	f => {
-			const name = f.split(".")[0] + ""
-			return name.replace("_",".")
-		}
-	 ).reverse()
+	 	f => 
+			versionedContentVersionFromFilename(f)
+		
+	 ).sort().reverse()
 	 let contentData = {
-		 payload: files
+		 payload: allVersions
 		}
 	 return [ allVersions,
 		 contentData]
