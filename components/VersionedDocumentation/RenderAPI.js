29d840f80fede7307bc81d297f80221af559b198
g.j.hilton <guyjohnhilton@gmail.com>
2025-01-01 15:43:24 +0000

    wip

--- components/VersionedDocumentation/RenderAPI.js
+++ components/VersionedDocumentation/RenderAPI.js
@@ -1,25 +1,54 @@
-import dynamic from 'next/dynamic';
-import {
-	jsonSchemaToDot,
-	jsonSchemaToDotB
-} from '@/utilities/jsonSchemaToDot'
-const Graphviz = dynamic(() => import('graphviz-react'), { ssr: false });
+//import dynamic from 'next/dynamic';
+/*import {
+	jsonSchemaToDot
+} from '@/utilities/jsonSchemaToDot' */
+//const Graphviz = dynamic(() => import('graphviz-react'), { ssr: false });
 
   
-
+/*
 export const RenderAPI = ({data}) => {
 	const dot = jsonSchemaToDot(data)
-
-	const workInProgress = jsonSchemaToDotB(data)
-
 return (<div>
-{/*<Graphviz dot={dot}
+<Graphviz dot={dot}
 options ={{
-	height: 1080,
+	height: 9000,
 width: 1028,
 zoom: false
- }} />*/}
-	<code><pre>{JSON.stringify(workInProgress, undefined, 2)}</pre></code>
+ }} />
+
+	<code><pre>{JSON.stringify(data.components.schemas, undefined, 2)}</pre></code>
 </div>)
 
+}
+
+*/
+
+import styles from "./RenderAPI.module.css"
+
+const Method = ({methodName,data}) => <div className={styles.method}>
+<h3>{methodName}</h3>
+{JSON.stringify(data)}
+	</div>
+
+const Path = ({pathName,data}) => <div className={styles.path}>
+	<h2>{pathName}</h2>
+		{
+		Object.keys(data).map(
+			k => <Method key={k} methodName={k} data={data[k]}/>
+			)
+		}
+	</div>
+
+export const RenderAPI = ({data}) => {
+	const paths = data.paths
+	return (
+		<div>
+		{
+			Object.keys(data.paths).map(
+				k => <Path key={k} pathName={k} data={data.paths[k]} />
+			)
+		}
+	{/*<code><pre>{JSON.stringify(data.paths, undefined, 2)}</pre></code> */}
+	</div>
+	)
 }
--- utilities/jsonSchemaToDot.js
+++ utilities/jsonSchemaToDot.js
@@ -8,105 +8,12 @@ const HEADER_FG_COLOUR = "#ffffff"
 const ROW_BG_COLOUR = "#fafafa"
 const DESCRIPTION_FG_COLOUR = "#777777"
 
-const tableOne = {
-    name:"wang",
-    rows:[
-        {
-            name:"id",
-            datatype:"int",
-            description:"This is a description."
-        },
-        {
-            name:"name",
-            datatype:"varchar",
-            description:"This is a prettty darn long description. which will need to wrap"
-        },
-
-        {
-            name:"last_update",
-            datatype:"timestamp",
-            description:"This is a description."
-        },
-    ]
-}
-
-const tableTwo = {
-    name:"chung",
-    rows:[
-        {
-            name:"id",
-            datatype:"int",
-            description:"This is a description."
-        },
-        {
-            name:"name",
-            datatype:"varchar",
-            description:"This is a prettty darn long description. which will need to wrap"
-        },
-        {
-            name:"wang_id",
-            datatype:"int"
-        },
-        {
-            name:"title",
-            datatype:"varchar",
-            description:"This is short"
-        },
-        {
-            name:"last_update",
-            datatype:"timestamp",
-            description:"This is a description."
-        },
-    ]
-}
-
-const tableThree = {
-    name:"everybody",
-    rows:[
-        {
-            name:"id",
-            datatype:"int",
-            description:"This is a description."
-        },
-        {
-            name:"wang_id",
-            datatype:"int"
-        },
-        {
-            name:"chung_id",
-            datatype:"int"
-        }
-    ]
-}
-
-const tables = [tableOne, tableTwo, tableThree]
-
-
-const description = text => {
-    const allLines = wrapString(text,30)
-    return  allLines.map(line => `<TR><TD COLSPAN="2" ALIGN="LEFT"><FONT COLOR="${DESCRIPTION_FG_COLOUR}">${line}</FONT></TD>}</TR>`).join(' ')
-}
-
-const row = (data,n) =>  `<TR>
-<TD ALIGN="LEFT" PORT="f${n}" BGCOLOR="${ROW_BG_COLOUR}">
-    <TABLE CELLPADDING="0" CELLSPACING="0" BORDER="0">
-        <TR>
-            <TD ALIGN="LEFT"><B>${data.name}</B></TD>
-            <TD ALIGN="RIGHT"><I>${data.datatype}</I></TD>
-        </TR>
-        ${data.description && description(data.description)}
-    </TABLE>
-</TD>
-</TR>`
-
-const table = data => `
-    "${data.name}" [
-        id="${data.name}";
-        label=<<TABLE BORDER="2" COLOR="${TEXT_COLOUR}" CELLBORDER="1" CELLSPACING="0" CELLPADDING="10">
-        <TR><TD PORT="f0" BGCOLOR="${HEADER_BG_COLOUR}"><FONT COLOR="${HEADER_FG_COLOUR}"><B>                    ${data.name}                  </B></FONT></TD></TR>
-        ${data.rows.map((rowData,n) => row(rowData,n+1)).join(" ")}
-    </TABLE>>];
-    `
+const CONFIG = `
+      rankdir=LR;
+      graph [fontname="helvetica", fontsize=32, fontcolor="#000", bgcolor="transparent"];
+      node [penwidth=0, margin=0, fontname="helvetica", fontsize=32, fontcolor="#000"];
+      edge [fontname="helvetica", fontsize=32, fontcolor="#000000", color="#000000"];
+`
 
 const connection = ({
     fromTable, 
@@ -115,90 +22,56 @@ const connection = ({
     toPort
 }) =>  `"${fromTable}":f${fromPort}:e -> "${toTable}":f${toPort}:w [dir=forward, penwidth=3, color="#000"]`
 
-const CONFIG = `
-      rankdir=LR;
-      graph [fontname="helvetica", fontsize=32, fontcolor="#000", bgcolor="transparent"];
-      node [penwidth=0, margin=0, fontname="helvetica", fontsize=32, fontcolor="#000"];
-      edge [fontname="helvetica", fontsize=32, fontcolor="#000000", color="#000000"];
-`
 
 
-export const jsonSchemaToDot = schema => {
-    let dot = `
-digraph dbml {
-      ${CONFIG}
-${tables.map(t=>table(t)).join(' ' )}
-
-${connection({
-    fromTable:"chung", 
-    fromPort:3,
-    toTable:"wang",
-    toPort:1
-})}
-      
-
-${connection({
-    fromTable:"everybody", 
-    fromPort:2,
-    toTable:"wang",
-    toPort:1
-})}
-
-${connection({
-    fromTable:"everybody", 
-    fromPort:3,
-    toTable:"chung",
-    toPort:1
-})}
-
-    }
-`     
-
-    return dot
-
-
+const description = text => {
+    const allLines = wrapString(text,30)
+    return  allLines.map(line => `<TR><TD COLSPAN="2" ALIGN="LEFT"><FONT COLOR="${DESCRIPTION_FG_COLOUR}">${line}</FONT></TD>}</TR>`).join(' ')
 }
 
+const table = (schema,name) => `
+    "${name}" [
+        id="${name}";
+        label=<<TABLE BORDER="2" COLOR="${TEXT_COLOUR}" CELLBORDER="1" CELLSPACING="0" CELLPADDING="10">
+        <TR><TD PORT="f0" BGCOLOR="${HEADER_BG_COLOUR}"><FONT COLOR="${HEADER_FG_COLOUR}"><B>                    ${name}                  </B></FONT></TD></TR>
+        ${Object.keys(schema.properties).map((key,n) => row(key,schema.properties[key],n+1)).join(" ")}
+    </TABLE>>];
+    `
 
+    const row = (name,property,n) =>  `<TR>
+    <TD ALIGN="LEFT" PORT="f${n}" BGCOLOR="${ROW_BG_COLOUR}">
+        <TABLE CELLPADDING="0" CELLSPACING="0" BORDER="0">
+            <TR>
+                <TD ALIGN="LEFT"><B>${name}</B></TD>
+                <TD ALIGN="RIGHT"><I>${property.type}</I></TD>
+            </TR>
+            ${property.description && description(property.description)}
+        </TABLE>
+    </TD>
+    </TR>`
 
-
-
-export const jsonSchemaToDotB = (data) => {
-    data = Object.assign(data,{})
+const makeTables = data => {
     const components = data.components
     const schemas = components.schemas
-    const keys = Object.keys(schemas)
-    const objects = []
-
-    const processCandidate = (candidate,key) => {
-        if (candidate.type === "object") {
-            const candidateCopy = Object.assign(candidate,{})
-            candidateCopy.title=key
-            if (candidateCopy.properties) {
-                const propertyKeys = Object.keys(candidateCopy.properties)
-                propertyKeys.forEach(
-                    pKey => {
-                        const property = candidateCopy.properties[pKey]
-                        const found = processCandidate(property,pKey)
-                        if (found) {
-                            delete(candidateCopy.properties[pKey])
-                        }
-                    }
-                )
-            }
-            delete(candidateCopy.required)
-            objects.push(candidateCopy)
-            return true
+    return Object.keys(schemas).map(
+        key => {
+            const schema = schemas[key]
+            let str = table(schema,key)
+            return(str)
         }
-        return false
-    }
-
-   keys.forEach(
-    key => {
-        const candidate = schemas[key]
-        processCandidate(candidate,key)
-    }
-   )
-    
-    return objects
+    ).join(' ')
 }
+
+export const jsonSchemaToDot = (data) => `
+        digraph dbml {
+            ${CONFIG}
+            ${makeTables(data)}
+            ${connection({
+                fromTable:"location_details", 
+                fromPort:3,
+                toTable:"organization_details",
+                toPort:1
+            })}
+        }
+    `   
+    
