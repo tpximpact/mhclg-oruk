9ea4b489c6168e15020f6bddfd7c183e9f2894d7
g.j.hilton <guyjohnhilton@gmail.com>
2025-01-21 21:22:15 +0000

    improve nowarranty

--- components/NoWarranty/index.js
+++ components/NoWarranty/index.js
@@ -8,11 +8,15 @@ export const NoWarranty = () => (
 				background: 'var(--VersionedDocumentation-legacy-background)',
 				color: 'var(--VersionedDocumentation-legacy-color)',
 				padding: '2rem',
-				fontWeight: '900'
+				fontWeight: '500',
+				marginTop: '1rem'
 			}}
 		>
-			Note: This is pre-release software. While we believe it to be correct to the best of our
-			knowledge, we make no warraties about its accuracy or quality. Please use with discretion. If
+			Note: <strong style={{
+		
+				fontWeight: '900'
+			}}>This is pre-release software.</strong> While we believe it to be correct to the best of our
+			knowledge, we make no warraties about accuracy, quality or fitness for any purpose. Please use with discretion. If
 			you would like to report bugs please do so by adding an issue at
 			https://github.com/tpximpact/mhclg-oruk/issues if you have permissions or by emailing us.
 			Thanks! - ORUK team
