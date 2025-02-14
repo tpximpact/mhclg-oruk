3bc68861246cf59abeb4a74e482c1ab4e6fcd04a
Guy Hilton <gjhltn@users.noreply.github.com>
2025-02-14 17:20:56 +0000

    move to

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
+import ToastProvider from '@/components/toast-provider';
 import { MessageCreateForm } from '@/components/message-create-form';
 import { MessageList } from '@/components/message-list';
 
 export default function Home() {
   return (
-    <main className="p-4">
+  <ToastProvider>
+    <main>
       <MessageCreateForm />
       <MessageList />
     </main>
+	</ToastProvider>
   );
 }
