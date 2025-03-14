20c9a2f7ce98a8868b3f5da9c9ba44e142b48d83
Guy Hilton <gjhltn@users.noreply.github.com>
2025-03-14 11:58:50 +0000

    dynamic e2e tests

--- e2e/pagesReachable.spec.ts
+++ e2e/pagesReachable.spec.ts
@@ -1,14 +1,41 @@
+import {flattenSite} from  '../src/utilities/flattenSite';
+import {slugify} from '../src/utilities/dynamicSection';
+import fs from 'fs'
+import { join } from 'path'
 const { test, expect } = require('@playwright/test');
 
-const data = [
-  {
-    path: "/",
-    lookFor: "Open"
-  }
-];
+const getAllFiles = contentFolder => {
+	const dir = join(process.cwd(), '/content', contentFolder)
+	const dirents = fs.readdirSync(dir, { withFileTypes: true })
+	return dirents
+		.filter(dirent => dirent.isFile())
+		.map(dirent => dirent.name)
+		.filter(f => f !== '.DS_Store')
+	}
+
+const site = flattenSite().filter(p=>!p.offsite)
+const nonDynamicPages = site.map(
+   page => ({
+       path: '/' + (page.urlPath ? page.urlPath : ""),
+	   lookFor: page.e2eTestLookFor
+   })
+)
+
+
+const dynamicPages = site
+  .filter(p => p.dynamic)
+  .flatMap(root => 
+    getAllFiles(root.contentPath).map(n => ({
+      path: `${root.contentPath}/${slugify(n)}`
+    }))
+  );
+
+let data = nonDynamicPages.concat(dynamicPages).filter((value, index, self) => 
+  self.findIndex((v) => v.path === value.path) === index
+);
 
 test.describe('Page Content Validation Tests', () => {
-  data.forEach(({ path, lookFor }) => {
+  data.forEach(({ path, lookFor = "open" }) => {
     test(`should find "${lookFor}" on ${path}`, async ({ page }) => {
       await page.goto(path);
 
