48e718656f485bc5ae8e327e2c587889a3040dd8
g.j.hilton <guyjohnhilton@gmail.com>
2025-03-22 01:27:31 +0000

    getNamedSiteItem

--- src/utilities/getNamedSiteItem.js
+++ src/utilities/getNamedSiteItem.js
@@ -1,11 +1,5 @@
 import { flattenSite } from './flattenSite'
 
-/**
- * Retrieves a site item by its name.
- *
- * @param {string} name - The name of the site item to retrieve.
- * @returns {Object} The site item with the specified name.
- */
 export const getNamedSiteItem = name => {
 	const r = flattenSite().filter(item => item.name === name)[0]
 	return r
