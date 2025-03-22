272a4e2976a033b536aa072f6cc1d7f8f3a032b4
g.j.hilton <guyjohnhilton@gmail.com>
2025-03-22 04:50:23 +0000

    add e2e fail case validation test

--- src/e2e/validate.spec.ts
+++ src/e2e/validate.spec.ts
@@ -1,21 +1,35 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, Page } from '@playwright/test'
 
-test('validates the API endpoint URL successfully', async ({ page }) => {
+async function setupValidatorPage(page: Page) {
 	await page.goto('http://192.168.1.198:3000/developers/validator')
 	await page.waitForSelector('h1:has-text("Check your ORUK compliance")')
 
 	const inputField = page.getByRole('textbox', { name: 'URL' })
 	await expect(inputField).toBeVisible()
 
-	const apiUrl = 'https://oruk-api-2a920f51d6bb.herokuapp.com/api/mock'
-	await inputField.fill(apiUrl)
-
 	const validateButton = page.getByRole('button', { name: 'Check' })
 	await expect(validateButton).toBeVisible()
-	await validateButton.click()
 
+	return { inputField, validateButton }
+}
+
+async function validateApiEndpoint(page: Page, apiUrl: string, expectedResult: 'Pass' | 'Fail') {
+	const { inputField, validateButton } = await setupValidatorPage(page)
+
+	await inputField.fill(apiUrl)
+	await validateButton.click()
 	await page.waitForLoadState('networkidle')
 
 	const successIndicator = page.locator('#overallResult')
-	await expect(successIndicator).toContainText('Pass', { timeout: 10000 })
+	await expect(successIndicator).toContainText(expectedResult, { timeout: 10000 })
+}
+
+test('validates the API endpoint URL for pass', async ({ page }) => {
+	const apiUrl = 'https://oruk-api-2a920f51d6bb.herokuapp.com/api/mock'
+	await validateApiEndpoint(page, apiUrl, 'Pass')
+})
+
+test('validates the API endpoint URL for fail', async ({ page }) => {
+	const apiUrl = 'https://oruk-api-2a920f51d6bb.herokuapp.com/api/mock/fail'
+	await validateApiEndpoint(page, apiUrl, 'Fail')
 })
