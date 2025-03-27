caae18165165fee7cacc804433d899cbd5121bd6
g.j.hilton <guyjohnhilton@gmail.com>
2025-03-27 10:57:24 +0000

    work around middleware useActionState bug

--- src/middleware.js
+++ src/middleware.js
@@ -1,35 +1,50 @@
 import { NextResponse } from 'next/server'
 
 const redirects = {
-  'validator.openreferraluk.org': '/developers/validator',
-  'developers.openreferraluk.org': '/developers',
-  'docs.openreferraluk.org': '/developers/api',
-  'taxonomy.openreferraluk.org': '/developers/overview',
-  'schema.openreferraluk.org': '/developers/schemata'
-};
+	'validator.openreferraluk.org': '/developers/validator',
+	'developers.openreferraluk.org': '/developers',
+	'docs.openreferraluk.org': '/developers/api',
+	'taxonomy.openreferraluk.org': '/developers/overview',
+	'schema.openreferraluk.org': '/developers/schemata'
+}
 
 export function middleware(request) {
-	const url = new URL(request.url);
-	
+	const url = new URL(request.url)
+
 	if (url.hostname === 'forum.openreferraluk.org') {
 		return NextResponse.redirect(new URL('/', 'https://forum.openreferral.org'))
 	}
-	
-	const targetPath = redirects[url.hostname];
+
+	const targetPath = redirects[url.hostname]
 	if (targetPath) {
 		return NextResponse.redirect(new URL(targetPath, 'https://openreferraluk.org'))
 	}
 
+	// TH next bit splices the current path in to the headers.
 	// see https://www.propelauth.com/post/getting-url-in-next-server-components
+
+	// There's a known bug in middlewares which manipulate headers which causes useActionState to fail.
+	// Workaround is from https://github.com/vercel/next.js/issues/50659#issuecomment-1846046743
 	const p = request.nextUrl.pathname
-	const headers = new Headers(request.headers)
+	let headers = new Headers(request.headers)
 	headers.set('x-current-path', p)
-	return NextResponse.next({ headers })
+	const response = NextResponse.next({
+		request: {
+			headers: headers
+		}
+	})
+	return response
 }
 
 export const config = {
 	matcher: [
-		// match all routes except static files and APIs
-		'/((?!api|_next/static|_next/image|favicon.ico).*)'
+		{
+			source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+			missing: [
+				{ type: 'header', key: 'next-router-prefetch' },
+				{ type: 'header', key: 'next-action' },
+				{ type: 'header', key: 'purpose', value: 'prefetch' }
+			]
+		}
 	]
 }
