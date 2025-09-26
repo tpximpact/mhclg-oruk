fa89a1e5fc9e09bf3c27e213ebfacbf287ab0522
jeffcumpsty-tpx <jeff.cumpsty@tpximpact.com>
2025-09-26 19:58:19 +0100

    Fix feedback baner

--- src/app/adopt/use-cases/_components/feedback.tsx
+++ src/app/adopt/use-cases/_components/feedback.tsx
@@ -5,17 +5,15 @@ import { FaRegComment } from 'react-icons/fa6'
 export default function Feedback() {
 	return (
 		<div className={styles.feedbackContainer}>
-			<blockquote>
-				<h1 className={styles.newPageText}>This is a new page</h1>
-				<Link
-					href='https://docs.google.com/forms/d/1JiSxJSOUguLZv-1myjg5neQW8aoAA_8ywiQKiHXEdPE/preview'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Help us improve it with your feedback
-				</Link>
-				<FaRegComment size='2em' />
-			</blockquote>
+			<h1 className={styles.newPageText}>This is a new page</h1>
+			<Link
+				href='https://docs.google.com/forms/d/1JiSxJSOUguLZv-1myjg5neQW8aoAA_8ywiQKiHXEdPE/preview'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				Help us improve it with your feedback
+			</Link>
+			<FaRegComment size='2em' />
 		</div>
 	)
 }
