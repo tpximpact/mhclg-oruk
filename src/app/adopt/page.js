06d138894555738345ad4750c6430e698012a918
jeffcumpsty-tpx <jeff.cumpsty@tpximpact.com>
2025-10-01 01:36:28 +0100

    update the adopt homepage with the new content

--- src/app/adopt/page.js
+++ src/app/adopt/page.js
@@ -3,14 +3,25 @@ import { listDynamicSection } from '@/utilities/dynamicSection'
 import { PageMargin } from '@/components/PageMargin'
 
 export default function Page() {
+	const data = [
+		{
+			title: 'How to adopt the ORUK standard',
+			path: 'how-to-adopt-the-oruk-standard',
+			slug: 'A quick guide to adopting the ORUK standard.'
+		},
+		{
+			title: 'Open Referral UK use cases',
+			path: 'use-cases',
+			slug: 'Explore practical applications and benefits of sharing data using the Open Referral UK standard.'
+		},
+		...listDynamicSection({
+			rootContentFolder: 'adopt'
+		})
+	]
+
 	return (
 		<PageMargin>
-			<DynamicSectionListing
-				mainHeading='Adopt the standard in a council'
-				data={listDynamicSection({
-					rootContentFolder: 'adopt'
-				})}
-			/>
+			<DynamicSectionListing mainHeading='Adopt the standard in a council' data={data} />
 		</PageMargin>
 	)
 }
