a533f019888dbee852e11e0e180320f99b623117
Jeff Cumpsty <jeff@cumpsty.co.uk>
2025-11-06 15:18:56 +0000

    layout.js -> layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google'
+import { ReactNode } from 'react'
+import type { Metadata } from 'next'
 
 import '@/styles/reset.css'
 import '@/styles/tokens.css'
@@ -18,13 +20,13 @@ import { configValueToBoolean } from '@/utilities/configValueToBoolean'
 import { getInfoMenuItems } from '@/utilities/getInfoMenuItems'
 
 import { getRootLayoutItems } from '@/utilities/getRootLayoutItems'
+import { CookieProvider } from '@/components/CookieProvider'
 
 import { Analytics } from '@vercel/analytics/next'
-import { CookieProvider } from '@/components/CookieProvider'
 
 const font = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
 	generator: 'Next.js',
 	applicationName: 'Open Referral UK',
 	referrer: 'origin-when-cross-origin',
@@ -46,7 +48,11 @@ export const metadata = {
 	}
 }
 
-const Wrap = ({ children }) => (
+interface WrapProps {
+	children: ReactNode
+}
+
+const Wrap = ({ children }: WrapProps) => (
 	<html lang='en' id='html' className='no-js'>
 		<body className={`${font.className}`}>
 			{children}
@@ -55,7 +61,11 @@ const Wrap = ({ children }) => (
 	</html>
 )
 
-export default async function RootLayout({ children }) {
+interface RootLayoutProps {
+	children: ReactNode
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
 	// const headerList = await headers();
 	// const pathname = headerList.get("x-current-path");
 	// console.log ("--> " + pathname)
@@ -81,7 +91,6 @@ export default async function RootLayout({ children }) {
 					</LandmarkMain>
 				</div>
 				<LandmarkContentInfo
-					items={items}
 					infoItems={getInfoMenuItems()}
 					showNav={configValueToBoolean(process.env.USE_NAV)}
 				/>
