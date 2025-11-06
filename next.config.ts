19183387be5d41a9955a14810ea12e91dea61b9d
Jeff Cumpsty <jeff@cumpsty.co.uk>
2025-11-06 15:36:27 +0000

    Consolidate host/path redirects into TypeScript next.config.ts; remove next.config.mjs, src/middleware.js and src/redirects.json

---
+++ next.config.ts
@@ -0,0 +1,87 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+	experimental: {
+		serverComponentsHmrCache: false
+	},
+	images: {
+		dangerouslyAllowSVG: true
+	},
+	redirects: async () => {
+		return [
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'validator.openreferraluk.org'
+					}
+				],
+				destination: '/developers/validator',
+				permanent: true
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'developers.openreferraluk.org'
+					}
+				],
+				destination: '/developers',
+				permanent: true
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'docs.openreferraluk.org'
+					}
+				],
+				destination: '/developers/api',
+				permanent: true
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'taxonomy.openreferraluk.org'
+					}
+				],
+				destination: '/developers/overview',
+				permanent: true
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'schema.openreferraluk.org'
+					}
+				],
+				destination: '/developers/schemata',
+				permanent: true
+			},
+			{
+				source: '/:path*',
+				has: [
+					{
+						type: 'host',
+						value: 'forum.openreferraluk.org'
+					}
+				],
+				destination: 'https://forum.openreferral.org/',
+				permanent: true
+			},
+			{
+				source: '/dashboard',
+				destination: '/developers/dashboard',
+				permanent: true
+			}
+		]
+	}
+}
+
+export default nextConfig
