df07236b3ef514bd4b2ce5148173e8d8a74862f7
Jeff Cumpsty <jeff@cumpsty.co.uk>
2025-11-06 18:18:07 +0000

    Bump next to 16.0.1 and update yarn.lock; switch TSX to react-jsx and reformat tsconfig; add typescript.tsconfigPath to next.config.ts; replace CSS Modules @value breakpoint with literal 1024px across components to support NextJS JSX Runtime

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+	typescript: {
+		tsconfigPath: './tsconfig.json'
+	},
 	experimental: {
 		serverComponentsHmrCache: false
 	},
