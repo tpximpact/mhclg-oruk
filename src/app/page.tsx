010f245f01f8bdd97e114178e25f489aeb9933b3
Jeff Cumpsty <jeff@cumpsty.co.uk>
2026-01-16 00:46:16 +0100

    feat: Reorganize application structure by adding new action handlers and updating not-found and home pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Homepage } from '@/components/Homepage'
+import { Metadata } from 'next'
 
 import data from '../../content/home/index.json'
 
-export const metadata = {
-	title: 'Open Referral UK'
+export const metadata: Metadata = {
+  title: 'Open Referral UK'
 }
 
 export default function Home() {
-	return <Homepage data={data} />
+  return <Homepage data={data} />
 }
